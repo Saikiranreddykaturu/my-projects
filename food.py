3c76480ed3357fd0d98c0a90ea17a6e1a7d3be35
Saikiranreddykaturu <105559084+Saikiranreddykaturu@users.noreply.github.com>
2022-07-18 23:06:59 +0530

    Add files via upload

---
+++ food.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+import random
+
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(0.5, 0.5)
+        self.color("blue")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        random_x = random.randint(-280, 280)
+        random_Y = random.randint(-280, 200)
+        self.goto(random_x, random_Y)
+
+
+
+
